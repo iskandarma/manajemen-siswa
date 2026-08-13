a7a010d0ce990c2939863f0ceba6048a4d66cd43
Moh Iskandar Maulana <mohmaulana65@guru.smk.belajar.id>
2026-08-13 10:55:23 +0700

    tahap 2 - mampilkan data dari query

--- index.php
+++ index.php
@@ -14,39 +14,47 @@ $students = $stmt->fetchAll();
 
 <!DOCTYPE html>
 <html lang="id">
+
 <head>
     <meta charset="UTF-8">
     <title>Manajemen Siswa</title>
 </head>
+
 <body>
 
-    <h1>Daftar Siswa</h1>
+<h1>Daftar Siswa</h1>
 
-    <table border="1" cellpadding="10">
-        <thead>
+<table border="1" cellpadding="10">
+
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>NIS</th>
+            <th>Nama</th>
+            <th>Jenis Kelamin</th>
+            <th>Kelas</th>
+            <th>Jurusan</th>
+        </tr>
+    </thead>
+
+    <tbody>
+
+        <?php foreach ($students as $index => $student): ?>
+
             <tr>
-                <th>No</th>
-                <th>NIS</th>
-                <th>Nama</th>
-                <th>Kelas</th>
+                <td><?= $index + 1 ?></td>
+                <td><?= $student['nis'] ?></td>
+                <td><?= $student['name'] ?></td>
+                <td><?= $student['gender'] ?></td>
+                <td><?= $student['class_name'] ?></td>
+                <td><?= $student['major'] ?></td>
             </tr>
-        </thead>
 
-        <tbody>
+        <?php endforeach; ?>
 
-            <?php foreach ($students as $index => $student): ?>
+    </tbody>
 
-                <tr>
-                    <td><?= $index + 1 ?></td>
-                    <td><?= $student['nis'] ?></td>
-                    <td><?= $student['name'] ?></td>
-                    <td><?= $student['class_name'] ?></td>
-                </tr>
-
-            <?php endforeach; ?>
-
-        </tbody>
-    </table>
+</table>
 
 </body>
 </html>
