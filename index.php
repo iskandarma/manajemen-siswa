5a140b72ad181269f1d0556eadad955a74ca02b5
Moh Iskandar Maulana <mohmaulana65@guru.smk.belajar.id>
2026-08-13 08:47:17 +0700

    tahap 1 - tambah tabel

--- index.php
+++ index.php
@@ -18,3 +18,44 @@ $students = [
 foreach ($students as $student) {
     echo $student['name'];
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Manajemen Siswa</title>
+</head>
+<body>
+
+    <h1>Daftar Siswa</h1>
+
+    <table border="1" cellpadding="10">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>NIS</th>
+                <th>Nama</th>
+                <th>Kelas</th>
+            </tr>
+        </thead>
+
+        <tbody>
+
+            <?php foreach ($students as $index => $student): ?>
+
+                <tr>
+                    <td><?= $index + 1 ?></td>
+                    <td><?= $student['nis'] ?></td>
+                    <td><?= $student['name'] ?></td>
+                    <td><?= $student['class'] ?></td>
+                </tr>
+
+            <?php endforeach; ?>
+
+        </tbody>
+    </table>
+
+</body>
+</html>
