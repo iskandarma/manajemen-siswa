dcc5eac3a00c385576bb5238e27be770a9104f77
Moh Iskandar Maulana <mohmaulana65@guru.smk.belajar.id>
2026-08-13 11:09:49 +0700

    tahap 3 - menambahkan halaman detail siswa

---
+++ views/students/show.php
@@ -0,0 +1,21 @@
+<h1>Detail Siswa</h1>
+
+<p>
+    NIS:
+    <?= $student['nis'] ?>
+</p>
+
+<p>
+    Nama:
+    <?= $student['name'] ?>
+</p>
+
+<p>
+    Kelas:
+    <?= $student['class_name'] ?>
+</p>
+
+<p>
+    Jurusan:
+    <?= $student['major'] ?>
+</p>
