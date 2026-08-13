16699e48bb20df01750b1a13c4436abf28affa9a
Moh Iskandar Maulana <mohmaulana65@guru.smk.belajar.id>
2026-08-13 08:40:51 +0700

    tahap 1

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
 
 echo "Manajemen Siswa";
+
+$students = [
+    [
+        'nis' => '25001',
+        'name' => 'Andi',
+        'class' => 'XI RPL 1'
+    ],
+    [
+        'nis' => '25002',
+        'name' => 'Siti',
+        'class' => 'XI RPL 1'
+    ],
+];
+
+foreach ($students as $student) {
+    echo $student['name'];
+}
