9878691ce48703908c5b64fea96a604d0e0c0d66
Moh Iskandar Maulana <mohmaulana65@guru.smk.belajar.id>
2026-08-13 11:08:00 +0700

    tahap 3 - memisahkan view

--- index.php
+++ index.php
@@ -2,59 +2,12 @@
 
 require 'config/database.php';
 
-echo "Manajemen Siswa";
-
 $sql = "SELECT * FROM students";
 
 $stmt = $pdo->query($sql);
 
 $students = $stmt->fetchAll();
 
+require 'views/students/index.php';
+
 ?>
-
-<!DOCTYPE html>
-<html lang="id">
-
-<head>
-    <meta charset="UTF-8">
-    <title>Manajemen Siswa</title>
-</head>
-
-<body>
-
-<h1>Daftar Siswa</h1>
-
-<table border="1" cellpadding="10">
-
-    <thead>
-        <tr>
-            <th>No</th>
-            <th>NIS</th>
-            <th>Nama</th>
-            <th>Jenis Kelamin</th>
-            <th>Kelas</th>
-            <th>Jurusan</th>
-        </tr>
-    </thead>
-
-    <tbody>
-
-        <?php foreach ($students as $index => $student): ?>
-
-            <tr>
-                <td><?= $index + 1 ?></td>
-                <td><?= $student['nis'] ?></td>
-                <td><?= $student['name'] ?></td>
-                <td><?= $student['gender'] ?></td>
-                <td><?= $student['class_name'] ?></td>
-                <td><?= $student['major'] ?></td>
-            </tr>
-
-        <?php endforeach; ?>
-
-    </tbody>
-
-</table>
-
-</body>
-</html>
