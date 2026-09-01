c54663a4f7c7a49ef51dfee8ee33ea6a5b860976
Moh Iskandar Maulana <mohmaulana65@guru.smk.belajar.id>
2026-09-01 08:33:11 +0700

    Tahap 5 - Menerapkan Student model

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require 'config/database.php';
 require __DIR__ . '/models/Student.php';
 
+// Create an instance of the Student model 
 $studentModel = new Student($pdo);
 
 $students = $studentModel->getAll();
