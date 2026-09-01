8e0a3a0b8a9b6081b5a00892dd692444ee5c16bd
Moh Iskandar Maulana <mohmaulana65@guru.smk.belajar.id>
2026-09-01 10:23:43 +0700

    Tahap 5 - Melengkapi method dan pengujian

--- models/Student.php
+++ models/Student.php
@@ -30,4 +30,62 @@ class Student
 
         return $stmt->fetch();
     }
+
+    public function create($data)
+    {
+        $sql = "INSERT INTO students
+            (nis, name, gender, class_name, major, birth_date, birth_place)
+            VALUES
+            (:nis, :name, :gender, :class_name, :major, :birth_date, :birth_place)";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        return $stmt->execute([
+            'nis' => $data['nis'],
+            'name' => $data['name'],
+            'gender' => $data['gender'],
+            'class_name' => $data['class_name'],
+            'major' => $data['major'],
+            'birth_date' => $data['birth_date'],
+            'birth_place' => $data['birth_place']
+        ]);
+    }
+
+    public function update($id, $data)
+    {
+        $sql = "UPDATE students
+            SET
+                nis = :nis,
+                name = :name,
+                gender = :gender,
+                class_name = :class_name,
+                major = :major,
+                birth_date = :birth_date,
+                birth_place = :birth_place
+            WHERE id = :id";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        return $stmt->execute([
+            'id' => $id,
+            'nis' => $data['nis'],
+            'name' => $data['name'],
+            'gender' => $data['gender'],
+            'class_name' => $data['class_name'],
+            'major' => $data['major'],
+            'birth_date' => $data['birth_date'],
+            'birth_place' => $data['birth_place']
+        ]);
+    }
+
+    public function delete($id)
+    {
+        $sql = "DELETE FROM students WHERE id = :id";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        return $stmt->execute([
+            'id' => $id
+        ]);
+    }
 }
---
+++ test.php
@@ -0,0 +1,41 @@
+<?php
+
+require __DIR__ . '/config/database.php';
+require __DIR__ . '/models/Student.php';
+
+$studentModel = new Student($pdo);
+
+// $students = $studentModel->getAll();
+
+// $students = $studentModel->find(2031);
+// var_dump($students);
+
+// $result = $studentModel->create([
+//     'nis' => '25006',
+//     'name' => 'Doni Saputra',
+//     'gender' => 'L',
+//     'class_name' => 'XI RPL 1',
+//     'major' => 'Rekayasa Perangkat Lunak',
+//     'birth_date' => '2005-05-15',
+//     'birth_place' => 'Bandung',
+// ]);
+
+
+// $result = $studentModel->update(1, [
+//     'nis' => '25001',
+//     'name' => 'Andi Pratama Update',
+//     'gender' => 'L',
+//     'class_name' => 'XI RPL 2',
+//     'major' => 'Rekayasa Perangkat Lunak',
+//     'birth_date' => '2005-05-15',
+//     'birth_place' => 'Bandung'
+// ]);
+
+$result = $studentModel->delete(5);
+
+var_dump($result);
+$students = $studentModel->getAll();
+
+echo '<pre>';
+print_r($students);
+echo '</pre>';
